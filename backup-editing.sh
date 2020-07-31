a72daa120855c3275b30b0715f71251248d04425
T-Visor <turkimbro@yahoo.com>
2020-07-31 10:37:00 -0400

    working on new shell script 'backup-editing.sh'

---
+++ backup-editing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Source and target directories for backup
+TARGET_DIRECTORY=value
+SOURCE_DIRECTORY="/home/t-visor/Documents"
+
+# Console text colors
+RED='\033[1;33m'
+PURPLE='\033[1;35m'
+NO_COLOR='\033[0m'
+
+start_sync () {
+    TARGET_DIRECTORY=$1
+    echo -e "${RED}SOURCE: $SOURCE_DIRECTORY"
+    echo -e "${PURPLE}DESTINATION: $TARGET_DIRECTORY"
+    echo -e "${NO_COLOR}"
+    python3 sync.py -target $TARGET_DIRECTORY -source $SOURCE_DIRECTORY
+}
+
+# Find the backup drive path
+DRIVE=$(find /media/t-visor -name 'my-files')
+
+# start syncing process if the drive is connected 
+[ ! -z $DRIVE ] && start_sync $DRIVE || echo "Drive not connected."
